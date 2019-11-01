6333d20c09b4c37bed773abdae6221c15a5a555e
NomatulNijhum <57229621+NomatulNijhum@users.noreply.github.com>
2019-11-01 12:53:55 +0600

    v1
    v1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+            hms
                 </div>
 
                 <div class="links">
